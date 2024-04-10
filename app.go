86bb4ca2ef1325712f9fe9ec1b1c07dc2953e09e
Dheeraj Kumar <dkchalotra@gmail.com>
2024-04-10 23:58:58 +0530

    register patient repository with di container

--- app.go
+++ app.go
@@ -7,6 +7,7 @@ import (
 	"github.com/cod3rboy/practice-cqrs/db"
 	"github.com/cod3rboy/practice-cqrs/eventstore"
 	"github.com/cod3rboy/practice-cqrs/handlers"
+	"github.com/cod3rboy/practice-cqrs/repositories"
 	"github.com/cod3rboy/practice-cqrs/server"
 	"go.uber.org/fx"
 	"go.uber.org/fx/fxevent"
@@ -21,6 +22,7 @@ func SetupAndRun() {
 			db.NewDatabaseClient,
 			server.NewServer,
 			fx.Annotate(eventstore.NewPostgresEventStore, fx.As(new(eventstore.Store))),
+			fx.Annotate(repositories.NewPatientRepository, fx.As(new(repositories.PatientRepository))),
 		),
 		handlers.HandlersModule,
 		TemporalModule,
